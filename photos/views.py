5c79dcd662fce1a0a0f06e8870cab19a4ded410b
Doktatech <Doktatech2@gmail.com>
2019-06-16 12:56:05 +0300

    Update views to render templates

--- photos/views.py
+++ photos/views.py
@@ -1,20 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 import datetime as dt 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to  my photo gallery')
+    return render(request, 'welcome.html')
 
 def photos_of_day(request):
     date = dt.date.today()
-    day =convert_dates(date)
-    html= f''''
-        <html>
-            <body>
-            <h1> Photos of {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-        '''
+    day =convert_dates(date)    
     return HttpResponse(html)
 def convert_dates(dates):
     # Function that gets the weekday number for the date.
@@ -22,5 +15,13 @@ def convert_dates(dates):
     days= ["Moday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
     day =days[day_number]
     return day 
-def past_days_photos(request):
+def past_days_photos(request,past_date):
     # Function for getting archived photos 
+    try:
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 ERROR
+        raise Http404
+    day =convert_dates(date)
+    
+    return HttpResponse(html)
