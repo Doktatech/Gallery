72405d6205ba787dd49efefd0cdd5d946e615567
Doktatech <Doktatech2@gmail.com>
2019-06-16 17:14:34 +0300

    Create a photo admin class

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Uploader,Image,tags
+class PhotoAdmin(admin.ModelAdmin):
+    filter_horizontal=('tags',)
+
+admin.site.register(Uploader)
+admin.site.register(Image)
+admin.site.register(tags)
 
 # Register your models here.
