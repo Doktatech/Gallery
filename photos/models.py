0584a79683ae847ac1b4870f34cd594d2e91e443
Doktatech <Doktatech2@gmail.com>
2019-06-16 14:55:09 +0300

    Create an Image model

--- photos/models.py
+++ photos/models.py
@@ -4,3 +4,28 @@ class Uploader(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+    def __str__(self):
+        return self.first_name
+    class Meta:
+        ordering = ['first_name']
+    
+    # try:
+    #     uploader = Uploader.objects.get(email = 'example@gmail.com')
+    #     print('Uploader found')
+    # except DoesNotExist:
+    #     print('Uploader was not found')
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+    def __str__(self):
+        return self.name
+class Image(models.Model):
+    image_name= models.CharField(max_length=50)
+    uploader = models.ForeignKey(Uploader)
+    photo_image =models.ImageField(upload_to='photos/')
+    tags = models.ManyToManyField(tags)
+    image_description = models.TextField()    
+    upload_date = models.DateField(auto_now_add=True)
+    image_location= models.CharField(max_length=60)
+    image_category=models.CharField(max_length=50)
+
+
