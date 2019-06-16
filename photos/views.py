e3d5debe192fa038ed4bc06073e67eab8f80d981
Doktatech <Doktatech2@gmail.com>
2019-06-16 12:30:21 +0300

    Add a view function for photos of the day

--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return HttpResponse('Welcome to  my photo gallery')
 
-# Create your views here.
+def photos_of_day(request):
+    date = dt.date.today()
+    day =convert_dates(date)
+    html= f''''
+        <html>
+            <body>
+            <h1> Photos of {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+        '''
+    return HttpResponse(html)
+def convert_dates(dates):
+    # Function that gets the weekday number for the date.
+    day_number=dt.date.weekday(dates)
+    days= ["Moday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+    day =days[day_number]
+    return day 
